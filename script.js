3b6614db440868de8888b0f08a2f8a4f9eb91cfa
adhithraghunath006-stack <adhithraghunath006@gmail.com>
2026-06-07 17:20:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ==================== 🚀 CONFIGURATION ====================
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = " https://arns-arena.onrender.com";
 // const BASE_URL = "https://your-app.onrender.com"; 
 // ==========================================================
 
